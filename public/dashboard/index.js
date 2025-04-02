fab3de7f099d9b2b5460144feef9ab95bece46f4
isaacdenny <isaacquinndenny@gmail.com>
2025-04-02 12:33:15 -0400

    trying to send game message

--- public/dashboard/index.js
+++ public/dashboard/index.js
@@ -4,27 +4,6 @@ const websocket = new WebSocket("ws://localhost:4242/ws");
 // Must send after connections are open
 websocket.onopen = (event) => {
   console.log("Websocket successfully connected");
-
-  let board = [
-    ["0", "0", "1", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "0", "0", "0"],
-    ["3", "0", "0", "0", "0", "0", "0", "0"],
-    ["3", "0", "0", "0", "0", "0", "0", "0"],
-    ["3", "0", "0", "0", "0", "0", "2", "2"],
-  ];
-
-  let message = new Uint8Array(65);
-
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      message[i * 8 + j + 1] = board[i][j];
-    }
-  }
-  message[0] = 0xf0;
-  websocket.send(message);
 };
 
 websocket.onmessage = (event) => {
@@ -41,3 +20,23 @@ websocket.onclose = (event) => {
   console.log("Connection closed");
   console.log(event);
 };
+
+let board = [
+  ["0", "0", "1", "0", "0", "0", "0", "0"],
+  ["0", "0", "0", "0", "0", "0", "0", "0"],
+  ["0", "0", "0", "0", "0", "0", "0", "0"],
+  ["0", "0", "0", "0", "0", "0", "0", "0"],
+  ["0", "0", "0", "0", "0", "0", "0", "0"],
+  ["3", "0", "0", "0", "0", "0", "0", "0"],
+  ["3", "0", "0", "0", "0", "0", "0", "0"],
+  ["3", "0", "0", "0", "0", "0", "2", "2"],
+];
+
+let message = new Uint8Array(65);
+
+for (let i = 0; i < 8; i++) {
+  for (let j = 0; j < 8; j++) {
+    message[i * 8 + j + 1] = board[i][j];
+  }
+}
+message[0] = 0xf0;
